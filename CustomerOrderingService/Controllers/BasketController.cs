18fcc90a526e9a46a21ecaadd63aad5e9796d346
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2020-11-11 14:59:32 +0000

    Add project files.

---
+++ CustomerOrderingService/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomerOrderingService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(ILogger<BasketController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Get basket
+        [HttpPost]
+        public Task<IActionResult> Get(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Add item to basket
+        [HttpPost]
+        public Task<IActionResult> Create(int customerId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Edit product in basket
+        [HttpPut]
+        public Task<IActionResult> Edit(int customerId, int productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Remove item from basket
+        [HttpDelete]
+        public Task<IActionResult> Delete(int customerId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomerOrderingService/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerOrderingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public Task<IActionResult> Create(int customerId)
+        {
+            //create order, create FK relationships between order and product
+            //then delete all products from the customer's basket
+            throw new NotImplementedException();
+        }
+
+        //could also have edit order and cancel order however not in brief, implement later if time
+    }
+}
